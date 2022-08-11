fd801b0a9f7a31955ca975438b6acb3253479a19
April Huang <phuang@beaconfireinc.com>
2022-08-11 14:38:37 -0400

    Initial commit

---
+++ src/main/java/com/example/githubactionsjune/GithubActionsJuneApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsjune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsJuneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsJuneApplication.class, args);
+    }
+
+}

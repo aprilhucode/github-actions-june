9b581cfb2abf7f70614f7d8f249651f923885ac4
April Huang <phuang@beaconfireinc.com>
2022-08-11 14:53:37 -0400

    Write tests

---
+++ src/test/java/com/example/githubactionsjune/SampleTest.java
@@ -0,0 +1,18 @@
+package com.example.githubactionsjune;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+
+    @Test
+    void test1() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void test2() {
+        assertEquals(2, 2);
+    }
+}
